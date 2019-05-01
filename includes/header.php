4b8fa6ea4bbbe4788dd0689ef423a580db05ea39
fakorede <abiolafakorede@gmail.com>
2019-05-01 15:52:35 +0100

    including files

---
+++ includes/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MeTube</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
+    <script src="assets/js/commonActions.js"></script>
+</head>
+<body>
+
+    <div id="pageContainer">
+    
+        <div id="mastHeadContainer">
+            <button class="navShowHide">
+                <img src="assets/images/icons/menu.png">
+            </button>
+
+            <a class="logoContainer" href="index.php">
+                <img src="assets/images/icons/VideoTubeLogo.png" title="logo" alt="Site logo">
+            </a>
+
+            <div class="searchBarContainer">
+                <form action="search.php" method="GET">
+                    <input type="text" class="searchBar" name="term" placeholder="Search...">
+                    <button class="searchButton">
+                        <img src="assets/images/icons/search.png" title="search button" alt="search icon">
+                    </button>
+                </form>
+            </div>
+
+            <div class="rightIcons">
+                <a href="upload.php">
+                    <img src="assets/images/icons/upload.png" alt="upload" class="upload">
+                </a>
+                <a href="#">
+                    <img src="assets/images/profilePictures/default.png" alt="upload" class="upload">
+                </a>
+            </div>
+        </div>
+
+        <div id="sideNavContainer" style="display:none;">
+        
+        </div>
+
+        <div id="mainSectionContainer">
+        
+            <div id="mainContentContainer">
