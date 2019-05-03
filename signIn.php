db6d790e5e7faf2e9e4e820599025dadb80c12c3
fakorede <abiolafakorede@gmail.com>
2019-05-03 16:09:10 +0100

    sign in and sign up forms

---
+++ signIn.php
@@ -0,0 +1,45 @@
+<?php require_once("includes/config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>VideoTube</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
+
+</head>
+<body>
+
+    <div class="signInContainer">
+
+        <div class="column">
+
+            <div class="header">
+                <img src="assets/images/icons/VideoTubeLogo.png" title="logo" alt="Site logo">
+                <h3>Sign In</h3>
+                <span>to continue to VideoTube</span>
+            </div>
+
+            <div class="loginForm">
+                <form action="signIn.php">
+                    <input type="text" name="username" placeholder="Username" autocomplete="off" required>
+                    <input type="password" name="password" placeholder="Password" required>
+                    <input type="submit" value="SUBMIT" name="submitButton>
+                </form>
+            </div>
+
+            <a class="signInMessage" href="signUp.php">Need an account? Sign up here!</a>
+        
+        </div>
+    
+    </div>
+
+
+
+
+</body>
+</html>
