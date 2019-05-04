52e6a138ecb523808a8ff4b0bd98284805255b47
fakorede <abiolafakorede@gmail.com>
2019-05-04 21:29:23 +0100

    created user class methods

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -5,9 +5,43 @@ class User {
     private $con, $sqlData;
 
     public function __construct($con, $username) {
-        
+
         $this->con = $con;
 
+        $query = $this->con->prepare("SELECT * FROM users WHERE username = :un");
+        $query->bindParam(":un", $username);
+        $query->execute();
+        
+        $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
+
+    }
+
+    public function getUsername() {
+        return $this->sqlData["username"];
+    }
+
+    public function getFirstName() {
+        return $this->sqlData["firstName"];
+    }
+
+    public function getLastName() {
+        return $this->sqlData["lastName"];
+    }
+
+    public function getName() {
+        return $this->sqlData["firstName"] . " " . $this->sqlData["firstName"];
+    }
+
+    public function getEmail() {
+        return $this->sqlData["email"];
+    }
+
+    public function getProfilePic() {
+        return $this->sqlData["profilePic"];
+    }
+
+    public function getSignUpDate() {
+        return $this->sqlData["signUpDate"];
     }
 }
 
--- index.php
+++ index.php
@@ -5,6 +5,12 @@
     if(isset($_SESSION["userLoggedIn"])) {
         echo "Welcome, " . $_SESSION["userLoggedIn"];
     }
+
+    echo '<br>';
+
+    echo $userLoggedInObj->getUsername();
+    echo '<br>';
+    echo $userLoggedInObj->getName();
 ?>
 
 <?php require_once("includes/footer.php"); ?>
