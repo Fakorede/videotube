b31253656faf8612b45c4edb1aec44dafa447e23
fakorede <abiolafakorede@gmail.com>
2019-05-04 17:36:49 +0100

    implemented sign up functionality

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -3,13 +3,98 @@
 class Account {
 
     private $con;
+    private $errorArray = array();
 
     public function __construct($con) {
         $this->con = $con;
     }
 
     public function register($firstName, $lastName, $username, $email, $email2, $password, $password2) {
-        
+        $this->validateFirstName($firstName);  
+        $this->validateLastName($lastName);  
+        $this->validateUsername($username);  
+        $this->validateEmails($email, $email2);  
+        $this->validatePasswords($password, $password2);  
+
+        if(empty($this->errorArray)) {
+            return $this->insertUserDetails($firstName, $lastName, $username, $email, $password);
+        } else {
+            return false;
+        }
+    }
+
+    public function insertUserDetails($firstName, $lastName, $username, $email, $password) {
+        return true;
+    }
+
+    private function validateFirstName($firstName) {
+        if(strlen($firstName) > 25 || strlen($firstName < 2)) {
+            array_push($this->errorArray, Constants::$firstNameCharacters);
+        }
+    }
+
+    private function validateLastName($lastName) {
+        if(strlen($lastName) > 25 || strlen($lastName < 2)) {
+            array_push($this->errorArray, Constants::$lastNameCharacters);
+        }
+    }
+
+    private function validateUsername($username) {
+        if(strlen($username) > 25 || strlen($username < 5)) {
+            array_push($this->errorArray, Constants::$usernameCharacters);
+            return;
+        }
+
+        $query = $this->con->prepare("SELECT username FROM users WHERE username = :username");
+        $query->bindParam(":username", $username);
+        $query->execute();
+
+        if($query->rowCount() != 0) {
+            array_push($this->errorArray, Constants::$usernameTaken);
+        }
+    }
+
+    private function validateEmails($email, $email2) {
+        if($email != $email2) {
+            array_push($this->errorArray, Constants::$emailsDoNotMatch);
+            return;
+        }
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            array_push($this->errorArray, Constants::$emailInvalid);
+            return;
+        }
+
+        $query = $this->con->prepare("SELECT email FROM users WHERE email = :email");
+        $query->bindParam(":email", $email);
+        $query->execute();
+
+        if($query->rowCount() != 0) {
+            array_push($this->errorArray, Constants::$emailTaken);
+        }
+    }
+
+    private function validatePasswords($password, $password2) {
+        if($password != $password2) {
+            array_push($this->errorArray, Constants::$passwordsDoNotMatch);
+            return;
+        }
+
+        if(preg_match("/[^A-Za-z0-9]/", $password)) {
+            array_push($this->errorArray, Constants::$passwordNotAlphaNumeric);
+            return;
+        }
+
+        if(strlen($password) > 30 || strlen($password < 5)) {
+            array_push($this->errorArray, Constants::$passwordLength);
+        }
+
+    }
+
+    public function getError($error) {
+        if(in_array($error, $this->errorArray)) {
+            return "<span class='errorMessage'>$error</span>";
+        }
     }
 }
 
--- signUp.php
+++ signUp.php
@@ -1,7 +1,8 @@
 <?php 
 require_once("includes/config.php"); 
-require_once("includes/classes/FormSanitizer.php"); 
 require_once("includes/classes/Account.php"); 
+require_once("includes/classes/FormSanitizer.php"); 
+require_once("includes/classes/Constants.php"); 
 
 $account = new Account($con);
 
@@ -18,8 +19,18 @@ if(isset($_POST["submitButton"])) {
     $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
     $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
     
-    $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
+    $wasSuccessful = $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
 
+    if($wasSuccessful) {
+
+    }
+
+}
+
+function getInputValue($name) {
+    if(isset($_POST[$name])) {
+        echo $_POST[$name];
+    }
 }
 
 ?>
@@ -52,13 +63,25 @@ if(isset($_POST["submitButton"])) {
 
                 <form action="signUp.php" method="POST">
                     
-                <input type="text" name="firstName" placeholder="First name" autocomplete="off" required>
-                <input type="text" name="lastName" placeholder="Last name" autocomplete="off" required>
-                <input type="text" name="username" placeholder="Username" autocomplete="off" required>
+                <?php echo $account->getError(Constants::$firstNameCharacters); ?>
+                <input type="text" name="firstName" placeholder="First name" value="<?php getInputValue('firstName'); ?>" autocomplete="off" required>
 
-                <input type="email" name="email" placeholder="Email" autocomplete="off" required>
-                <input type="email" name="email2" placeholder="Confirm email" autocomplete="off" required>
+                <?php echo $account->getError(Constants::$lastNameCharacters); ?>
+                <input type="text" name="lastName" placeholder="Last name" value="<?php getInputValue('lastName'); ?>" autocomplete="off" required>
 
+                <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                <?php echo $account->getError(Constants::$usernameTaken); ?>
+                <input type="text" name="username" placeholder="Username" value="<?php getInputValue('username'); ?>" autocomplete="off" required>
+
+                <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+                <?php echo $account->getError(Constants::$emailInvalid); ?>
+                <?php echo $account->getError(Constants::$emailTaken); ?>
+                <input type="email" name="email" placeholder="Email" value="<?php getInputValue('email'); ?>" autocomplete="off" required>
+                <input type="email" name="email2" placeholder="Confirm email" value="<?php getInputValue('email2'); ?>" autocomplete="off" required>
+
+                <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
+                <?php echo $account->getError(Constants::$passwordNotAlphaNumeric); ?>
+                <?php echo $account->getError(Constants::$passwordLength); ?>
                 <input type="password" name="password" placeholder="Password" autocomplete="off" required>
                 <input type="password" name="password2" placeholder="Confirm password" autocomplete="off" required>
 
