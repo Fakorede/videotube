928fddb697ac8fea354dce034c01e9274a6d8291
fakorede <abiolafakorede@gmail.com>
2019-05-04 22:10:25 +0100

    created video class methods

--- includes/classes/Video.php
+++ includes/classes/Video.php
@@ -21,6 +21,42 @@ class Video {
         }
     }
 
+    public function getId() {
+        return $this->sqlData["id"];
+    }
+
+    public function getUploadedBy() {
+        return $this->sqlData["uploadedBy"];
+    }
+
+    public function getDescription() {
+        return $this->sqlData["description"];
+    }
+
+    public function getPrivacy() {
+        return $this->sqlData["privacy"];
+    }
+
+    public function getFilePath() {
+        return $this->sqlData["filePath"];
+    }
+
+    public function getCategory() {
+        return $this->sqlData["category"];
+    }
+
+    public function getUploadDate() {
+        return $this->sqlData["uploadDate"];
+    }
+
+    public function getViews() {
+        return $this->sqlData["views"];
+    }
+
+    public function getDuration() {
+        return $this->sqlData["duration"];
+    }
+
 }
 
 
