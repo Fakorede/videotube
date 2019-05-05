c9847e9cef9e035ad5abee6b3e887dbd643757f8
fakorede <abiolafakorede@gmail.com>
2019-05-05 02:19:41 +0100

    created class for video info section

--- watch.php
+++ watch.php
@@ -2,6 +2,7 @@
 
 require_once("includes/header.php");
 require_once("includes/classes/VideoPlayer.php");
+require_once("includes/classes/VideoInfoSection.php");
 
 if(!isset($_GET["id"])) {
     echo "ERROR 404. PAGE NOT FOUND!";
@@ -17,6 +18,9 @@ $video->incrementViews();
 <div class="watchLeftColumn">
     <?php 
         $videoPlayer = new VideoPlayer($video);
+        echo $videoPlayer->create(true);
+
+        $videoPlayer = new VideoInfoSection($con, $video, $userLoggedInObj);
         echo $videoPlayer->create(true);
     ?>
 </div>
