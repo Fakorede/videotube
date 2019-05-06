303362f5ae6b151d7bf6765c1b05f6debc70034f
fakorede <abiolafakorede@gmail.com>
2019-05-06 02:49:26 +0100

    updating text of subcribe button based on sub

--- assets/js/userActions.js
+++ assets/js/userActions.js
@@ -5,8 +5,17 @@ function subscribe(userTo, userFrom, button) {
   }
 
   $.post("ajax/subscribe.php", { userTo: userTo, userFrom: userFrom }).done(
-    function(data) {
-      console.log(data);
+    function(count) {
+      if (count != null) {
+        $(button).toggleClass("subscribe unsubscribe");
+
+        var buttonText = $(button).hasClass("subscribe")
+          ? "SUBSCRIBE"
+          : "SUBSCRIBED";
+        $(button).text(buttonText + " " + count);
+      } else {
+        alert("Sth went wrong!");
+      }
     }
   );
 }
