6bab9100307bfe9077a784a5c7188d37234faf11
fakorede <abiolafakorede@gmail.com>
2019-05-06 01:15:14 +0100

    database file

---
+++ videotube.sql
@@ -0,0 +1,264 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 06, 2019 at 02:14 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `videotube`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Film & Animation'),
+(2, 'Autos & Vehicles'),
+(3, 'Music'),
+(4, 'Pets & Animals'),
+(5, 'Sports'),
+(6, 'Travel & Events'),
+(7, 'Gaming'),
+(8, 'People & Blogs'),
+(9, 'Comedy'),
+(10, 'Entertainment'),
+(11, 'News & Politics'),
+(12, 'Howto & Style'),
+(13, 'Education'),
+(14, 'Science & Technology'),
+(15, 'Nonprofits & Activism');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dislikes`
+--
+
+CREATE TABLE `dislikes` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `commentId` int(11) NOT NULL,
+  `videoId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `commentId` int(11) NOT NULL,
+  `videoId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`id`, `username`, `commentId`, `videoId`) VALUES
+(48, 'testuser', 0, 14);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscribers`
+--
+
+CREATE TABLE `subscribers` (
+  `id` int(11) NOT NULL,
+  `userTo` varchar(50) NOT NULL,
+  `userFrom` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thumbnails`
+--
+
+CREATE TABLE `thumbnails` (
+  `id` int(11) NOT NULL,
+  `videoId` int(11) NOT NULL,
+  `filePath` varchar(250) NOT NULL,
+  `selected` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `thumbnails`
+--
+
+INSERT INTO `thumbnails` (`id`, `videoId`, `filePath`, `selected`) VALUES
+(1, 13, 'uploads/videos/thumbnails/13-5cca4794b1610.jpg', 1),
+(2, 13, 'uploads/videos/thumbnails/13-5cca4795059a2.jpg', 0),
+(3, 13, 'uploads/videos/thumbnails/13-5cca47954b3de.jpg', 0),
+(4, 14, 'uploads/videos/thumbnails/14-5ccdf848c7c8d.jpg', 1),
+(5, 14, 'uploads/videos/thumbnails/14-5ccdf84906b59.jpg', 0),
+(6, 14, 'uploads/videos/thumbnails/14-5ccdf84941a07.jpg', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(25) NOT NULL,
+  `lastName` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `signUpDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `profilePic` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstName`, `lastName`, `username`, `email`, `password`, `signUpDate`, `profilePic`) VALUES
+(1, 'Fakorede', 'Abiola', 'fabman', 'abiolafakorede@gmail.com', 'e9399cfbff48c4e20c8e185144dfc999430ff3c0819ac169b9c7e64fa97d438965aea528d9ceca742c2c0a16678defbba7d3866709dbd5e12f1b402071faa264', '2019-05-04 17:56:28', 'assets/images/profilePictures/default.png'),
+(4, 'Test', 'Test', 'testuser', 'test2@gmail.com', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', '2019-05-04 18:16:33', 'assets/images/profilePictures/default.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE `videos` (
+  `id` int(11) NOT NULL,
+  `uploadedBy` varchar(50) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `privacy` int(11) NOT NULL,
+  `filePath` varchar(250) NOT NULL,
+  `category` int(11) NOT NULL,
+  `uploadDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `views` int(11) NOT NULL,
+  `duration` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `videos`
+--
+
+INSERT INTO `videos` (`id`, `uploadedBy`, `title`, `description`, `privacy`, `filePath`, `category`, `uploadDate`, `views`, `duration`) VALUES
+(13, 'REPLACE_THIS', 'title', 'll', 0, 'uploads/videos/5cca4792692e2.mp4', 1, '2019-05-02 02:27:46', 0, '00:05'),
+(14, 'testuser', 'random vid', 'desc', 0, 'uploads/videos/5ccdf844c69cf.mp4', 1, '2019-05-04 21:38:28', 64, '00:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dislikes`
+--
+ALTER TABLE `dislikes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `thumbnails`
+--
+ALTER TABLE `thumbnails`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT for table `dislikes`
+--
+ALTER TABLE `dislikes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+--
+-- AUTO_INCREMENT for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `thumbnails`
+--
+ALTER TABLE `thumbnails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
