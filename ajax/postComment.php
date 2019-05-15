4507f9e37e7a03edfd72cd2a829b02de4d5af090
fakorede <abiolafakorede@gmail.com>
2019-05-15 21:43:36 +0100

    insterting comment into db

--- ajax/postComment.php
+++ ajax/postComment.php
@@ -3,7 +3,19 @@
 require_once("../includes/config.php");
 
 if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['videoId'])) {
-    echo "commented";
+    $query = $con->prepare("INSERT INTO comments(postedBy, videoId, responseTo, body) VALUES(:postedBy, :videoId, :responseTo, :body)");
+    $query->bindParam(":postedBy", $postedBy);
+    $query->bindParam(":videoId", $videoId);
+    $query->bindParam(":responseTo", $responseTo);
+    $query->bindParam(":body", $commentText);
+
+    $postedBy = $_POST['postedBy'];
+    $videoId = $_POST['videoId'];
+    $responseTo = $_POST['responseTo'];
+    $commentText = $_POST['commentText'];
+
+    $query->execute();
+
    
 } else {
     echo "One or more parameters not passed!";
